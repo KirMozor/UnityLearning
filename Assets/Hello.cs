12421e3823c278d1a178b3d1300ae8e57483d5a4
KirMozor <kirmozor@gmail.com>
2022-01-05 12:56:15 +0600

    2

---
+++ Assets/Hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 1;
+        print(x + 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
